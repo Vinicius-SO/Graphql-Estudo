4b5bdd6f56530d940d5bac244d8d47e9fc41350c
Vinicius-SO <vhisoares39@gmail.com>
2022-04-06 16:41:43 -0300

    Second Commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
-function App() {
+import { gql, useQuery } from "@apollo/client"
 
+interface User {
+  id:string
+  name:string
+}
+
+const GET_USER = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`
+ 
+
+function App() {
+  const { data, loading } = useQuery<{users:User[]}>(GET_USER)
+  if(loading) {
+    return <p>Carregando...</p>
+  }
   return (
-    <h1> Hello world</h1>
+    <ul>
+      {data?.users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
   )
 }
 
